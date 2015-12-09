86c21b477b9a3134d2b69d415110f059c0729ff9
DaniGate <daniel.garrido.terrats@gmail.com>
2015-12-09 11:49:15 +0100

    Top words now is a callable function

--- top_words/top_words.py
+++ top_words/top_words.py
@@ -1,20 +1,21 @@
-from sklearn.feature_extraction.text import CountVectorizer
+def top_words(textsample,lang='english'):
 
-fname = "Job - Mobile Data Analyst eDreams.txt"
-text = ""
-text = open(fname,"r").read()
-text = text.replace('\n',' ')
-words = []
+    from sklearn.feature_extraction.text import CountVectorizer
+    if lang == 'english':
+        vectorizer = CountVectorizer(stop_words=lang,lowercase=True)
+    if lang == 'spanish':
+        from nltk.corpus import stopwords
+        spanish_stopwords = stopwords.words('spanish')
+        vectorizer = CountVectorizer(encoding=u'utf-8',stop_words=spanish_stopwords,lowercase=True)
 
-for word in text.split():
-    words.append(word)
+    words = textsample.split()
+    matrix = vectorizer.fit_transform(words)
+    freqs = [(word, matrix.getcol(idx).sum()) for word, idx in vectorizer.vocabulary_.items()]
 
-vectorizer = CountVectorizer(stop_words='english',lowercase=True)
-matrix = vectorizer.fit_transform(words)
-freqs = [(word, matrix.getcol(idx).sum()) for word, idx in vectorizer.vocabulary_.items()]
+    # Display the most frequent words (those appearing more than 3 times)
+    freq_words = sorted (freqs, key = lambda x: -x[1])
+    for w,f in freq_words:
+        if f >= 3:
+            print w,f
 
-# Display the most frequent words (those appearing more than 3 times)
-freq_words = sorted (freqs, key = lambda x: -x[1])
-for w,f in freq_words:
-    if f >= 3:
-        print w,f
+    return
